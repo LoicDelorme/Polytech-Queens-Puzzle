78642086b55e0e7cff1887bef287281a2a200890
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-03-15 14:59:24 +0100

    Add main method

---
+++ src/fr/polytech/queenspuzzle/Launcher.java
@@ -0,0 +1,20 @@
+package fr.polytech.queenspuzzle;
+
+/**
+ * This class represents the launcher of the application.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public class Launcher {
+
+	/**
+	 * The entry of the application.
+	 * 
+	 * @param args
+	 *            Some arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
